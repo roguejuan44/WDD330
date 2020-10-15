b10ab6a81c4d424c492d5b70b3bcb99da35ca41c
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2020-10-15 14:38:39 -0600

    ok

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const links = [
     {label: "Week 2", url: "week2/wk2.html"},
     {label: "Week 3", url: "week3/wk3.html"},
     {label: "Week 4", url: "week4/week4.html"},
-    {label: "Week 5", url: "week4/week05.html"}
+    {label: "Week 5", url: "week5/week05.html"}
 
 ]
 
