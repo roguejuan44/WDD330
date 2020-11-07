66aa0763804b8ced277597291d572391c3c52449
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2020-11-07 13:23:05 -0700

    wk8

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const links = [
     {label: "Week 1", url: "week1/index.html"},
     {label: "Week 2", url: "week2/wk2.html"},
+    /*{label: "Week 2 Team Activity", url: "week2/team.html"},*/
     {label: "Week 3", url: "week3/wk3.html"},
+  /*  {label: "Week 3 Team Activity", url: "week3/wk3.html"},*/
     {label: "Week 4", url: "week4/week4.html"},
     {label: "Week 5", url: "week5/week05.html"},
     {label: "Week 6: To Do Project", url: "week5/ToDo/index.html"},
     {label: "Week 7", url: "week7/week7.html"},
+    {label: "Week 8", url: "week8/week8.html"},
 
 ]
 
